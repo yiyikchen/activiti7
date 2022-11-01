c70bfe780d81641a3847fbbb5dcf886542282fc0
chenchao <chenchao@qq.com>
2022-11-01 14:30:12 +0800

    注释语句

---
+++ src/test/java/com/ccool/activiti7/Activiti7ApplicationTests.java
@@ -0,0 +1,12 @@
+package com.ccool.activiti7;
+
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Activiti7ApplicationTests {
+
+    void contextLoads() {
+    }
+
+}
